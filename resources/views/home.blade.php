a2a76ad3728009c0e86b47fc380881276e781195
TullioTambone <tulliotambone97@gmail.com>
2023-06-08 17:13:40 +0200

    main fine

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,57 @@
 @extends( 'layout.app' )
 
 @section('titlePage')
-    Laravel App | Home
+    Laravel App | COMICS
 @endsection
 
 @section( 'content' )
 
-<h1>Pagina Home</h1>
+<div class="container-fluid" id="jumbotron">
+</div>
+<div class="container-fluid" id="card-section">
+    <div class="container card-section p-5">
+        <span id="current-series">
+            CURRENT SERIES
+        </span>
 
-<!-- @foreach( $comicsArray as $elem )
-    <h2 class="text-primary">{{$elem['title']}}</h2>
-@endforeach -->
+        <div class="row">
+            @foreach( $comicsArray as $elem )
+            <div class="my-card col-12 col-md-2 col-lg-2">
+                <img src="{{$elem['thumb']}}" alt="">
+                <h6>{{$elem['series']}}</h6>
+            </div>
+            @endforeach
+        </div>
+        <span id="load-more">LOAD MORE</span>
+    </div>
+    <div class="container-fluid" id="buy">
+        <div class="container p-4" id="buy-section">
+            <div class="row text-white justify-content-center">
+                <div class="col-2">
+                    <img src="/images/buy-comics-digital-comics.png" style="width: 40px;" alt="">
+                    DIGITAL COMICS
+                </div>
+                <div class="col-2">
+                    <img src="/images/buy-comics-merchandise.png" alt="">
+                    DC MERCHANDISE
+                </div>
+                <div class="col-2">
+                    <img src="/images/buy-comics-subscriptions.png" alt="">
+                    SUBSCRIPION
+                </div>
+                <div class="col-2">
+                    <img src="/images/buy-comics-shop-locator.png" style="width: 40px;" alt="">
+                    COMIC SHOP LOCATOR
+                </div>
+                <div class="col-2">
+                    <img src="/images/buy-dc-power-visa.svg" alt="">
+                    DC POWER VISA
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 @endsection
